c94fe46351293bff7f6a4db29c9617c18349c928
Relu12345 <aurel.coruian02@e-uvt.ro>
2023-10-02 01:02:38 +0300

    merge (sper)

--- Assets/Multiplayer/Scripts/ModeSetter.cs
+++ Assets/Multiplayer/Scripts/ModeSetter.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using TMPro;
+using Unity.Netcode.Transports.UTP;
 
 public class ModeSetter : MonoBehaviour
 {
+    [SerializeField] private TMP_InputField inputText;
+
     public void StartHost()
     {
         NetworkManager.Singleton.StartHost();
@@ -13,5 +17,6 @@ public class ModeSetter : MonoBehaviour
     public void StartClient()
     {
         NetworkManager.Singleton.StartClient();
+        NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = inputText.text;
     }
 }
