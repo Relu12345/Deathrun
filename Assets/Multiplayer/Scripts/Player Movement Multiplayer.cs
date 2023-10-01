1dbe4f137da8077eb5574ea4008c0279108744c5
Relu12345 <aurel.coruian02@e-uvt.ro>
2023-10-02 01:57:50 +0300

    si asta

--- Assets/Multiplayer/Scripts/Player Movement Multiplayer.cs
+++ Assets/Multiplayer/Scripts/Player Movement Multiplayer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.Netcode;
+using System.Collections;
 
 public class PlayerMovementMultiplayer : NetworkBehaviour
 {
@@ -20,10 +21,15 @@ public class PlayerMovementMultiplayer : NetworkBehaviour
     private float wallJumpingDuration = 0.2f;
     private Vector2 wallJumpingPower = new Vector2(8f, 8f);
 
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 16f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private AudioListener listener;
     [SerializeField] private Camera cam;
-
     public override void OnNetworkSpawn()
     {
         if (IsOwner)
@@ -40,6 +46,11 @@ public class PlayerMovementMultiplayer : NetworkBehaviour
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && canDash)
+        {
+            StartCoroutine(Dash());
         }
 
         WallSlide();
@@ -81,10 +92,31 @@ public class PlayerMovementMultiplayer : NetworkBehaviour
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         if (!isWallJumping)
         {
             rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
         }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        if (Input.GetKey(KeyCode.A)) rb.velocity = new Vector2(-1 * dashingPower, 0f);
+        else rb.velocity = new Vector2(1 * dashingPower, 0f);
+        rb.transform.localScale = new Vector3(rb.transform.localScale.x, 0.5f, 1f);
+        rb.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y - 0.5f, 0f);
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        rb.transform.localScale = new Vector3(rb.transform.localScale.x, 1f, 1f);
+        rb.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y + 0.5f, 0f);
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 
     private bool IsGrounded()
